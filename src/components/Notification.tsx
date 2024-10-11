bebdcab5f54dab638c7ba99fe4c51e10b9007076
vijay kumar <49490763+rvijaykr10@users.noreply.github.com>
2024-10-11 04:28:44 +0000

    notification added

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Notification.css";
+
+const Notification: React.FC = () => {
+  return (
+    <div className="notification">
+      Please be patient, API uptime is 1 min during inactive !
+    </div>
+  );
+};
+
+export default Notification;
